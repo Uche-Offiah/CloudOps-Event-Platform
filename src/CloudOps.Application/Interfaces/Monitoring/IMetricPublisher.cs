f558c63bb66cadc8a4a79c5571ee7b1111aa00ae
uche <ucheoffiah@gmail.com>
2026-07-30 20:20:23 +0000

    feat(observability): publish CloudWatch custom metrics

---
+++ src/CloudOps.Application/Interfaces/Monitoring/IMetricPublisher.cs
@@ -0,0 +1,6 @@
+
+namespace CloudOps.Application.Interfaces.Monitoring;
+public interface IMetricPublisher
+{
+    Task PublishCountAsync(string metricName, double value, CancellationToken cancellationToken);
+} 
